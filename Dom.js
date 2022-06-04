da1d63321aeb95221f581762d498537a8c266b8b
Anurag Verma <av2anurag@gmail.com>
2022-06-05 01:02:20 +0530

    DOM Manipulation practise

---
+++ Dom.js
@@ -0,0 +1,16 @@
+//console.dir(document)
+//console.log(document.all);
+//document.all[10].textContent="Hello, Anurag";
+
+//console.log(document.getElementById('main'));
+//console.log(document.getElementById('header-title'));
+let headertitle=document.getElementById('header-title');
+headertitle.textContent="Hello, Anurag Verma"
+headertitle.style.borderBottom='solid 2px black';
+
+let mainhead=document.getElementById('main-header');
+mainhead.style.borderBottom='solid 4px black';
+
+let header2=document.getElementById('header 2');
+header2.style.color='green';
+header2.style.fontWeight='bold';
